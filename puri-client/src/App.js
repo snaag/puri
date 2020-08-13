9bb217387215b0c944150f4b25636a2aa574c2b7
CreamPuffx2 <exploit017@gmail.com>
2020-08-13 15:53:36 +0900

    App.js/Puri_logos UPDATE 헤더 작성

--- puri-client/src/App.js
+++ puri-client/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import puri_logo from "./image/Puri_logo_black.png";
+import styled, { css } from "styled-components";
+
+const Header = styled.div`
+  background: black;
+  height: 4rem;
+  margin: 0px;
+  border: 0px;
+`;
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Header>
+        <a href="#">
+          <img src={puri_logo} alt="Puri logo" width="100px" />
+        </a>
+      </Header>
+    </div>
+  );
 }
 
 export default App;
