4e238f0816220c31730158cbdedaa0ee2f6d758a
snaag <04_miffy@naver.com>
2020-08-14 11:08:52 +0900

    [MAKE, UPDATE] RDS에 DB 만듦 * MAKE, UPDATE: server/

---
+++ server/models/notes.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Notes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Notes.init({
+    id: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER,
+    picUrl: DataTypes.STRING,
+    resultText: DataTypes.STRING,
+    comment: DataTypes.STRING,
+    review: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Notes',
+  });
+  return Notes;
+};
---
+++ server/models/users.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Users.init({
+    id: DataTypes.INTEGER,
+    userId: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return Users;
+};
