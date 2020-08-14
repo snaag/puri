77b46ea08e7c6f1085dcc02fce31e51ef3506162
Myunggyun <sonmg90@gmail.com>
2020-08-14 17:04:07 +0900

    setup component main page

--- puri-client/src/components/Index/index.js
+++ puri-client/src/components/Index/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Puri_logo from '../../image/Puri_logo.png'
 
 function Index() {
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
   return (
     <div className="index-page">
       <img src={Puri_logo} className="puri_logo" width="100" height="100" alt="logo"/>
-      <form>
+      <form onSubmit={handleSubmit}>
         <button>구글로그인</button>
-      </form>  
+      </form>
     </div>
   );
 }
