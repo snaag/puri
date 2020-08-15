b884761abb5a04ce5d2a3cbfbe78cfc5bbda258d
CreamPuffx2 <exploit017@gmail.com>
2020-08-16 08:25:54 +0900

    index.js UPDATE 네이게이션 컴포넌트 연결

--- puri-client/src/components/index.js
+++ puri-client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Index } from './Index/Index';
 export { default as Main } from './Main/Main';
 export { default as Result } from './Result/Result';
+export { default as Navigation } from './Navigation/Navigation';
