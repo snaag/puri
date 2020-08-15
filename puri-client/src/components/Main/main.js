7bf2a2959abf0674c331fdd97623f3760309676c
Myunggyun <sonmg90@gmail.com>
2020-08-15 10:11:45 +0900

    modified resister button

--- puri-client/src/components/Main/main.js
+++ puri-client/src/components/Main/main.js
@@ -1,56 +1,103 @@
 import React, { useState, useEffect } from 'react';
 import Puri_logo from '../../image/Puri_logo.png';
-import Example from "../../image/example.jpg";
+import Example from "../../image/example1.jpg";
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 function Main() {
-  const [img, setImage] = useState(null)
-  const [value, setValue] = useState("풀이과정등록")
+  const [img, setImage] = useState(null);
+  const [value, setValue] = useState("풀이과정등록");
+  const [result, setResult] = useState(false);
 
-  const uploadFile = async (e) => {
-    let reader = new FileReader()
+  const uploadFile = e => {
+    let reader = new FileReader();
     let file = e.target.files[0];
     reader.onloadend = function () {
-      setImage(reader.result)
-    }
-    reader.readAsDataURL(file)
-    await handlebutton()
-  }
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
 
   const handlebutton = () => {
-    setValue("Solution")
-  }
+    setValue("Solution");
+  };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const data = new FormData()
-    data.append('file', img)
-    console.log(1)
-    // return await axios.post('/', data).then(res=>console.log(res))
-  }
+    const data = new FormData();
+    data.append("file", img);
+    console.log(1);
+    // await axios.post(url, data) //url과 객체형태의 data = {}
+    // .then(res=>console.log(res.statusText))
+    // await <Route path='/result' />
+    await handleRoute();
+  };
 
-  useEffect(()=>{
-  })
+  const handleRoute = () => {
+    setResult(!result);
+  };
+
+  // useEffect(()=>{
+  // })
 
   return (
     <div className="main-page">
-      <div><img src={Puri_logo} className="puri_logo" width="100" height="100" alt="logo"/></div>
-      {img===null ? (
-        <img src={Example} className="upload_picture" width="200" height="320" alt="upload"/>
+      <div>
+        <img
+          src={Puri_logo}
+          className="puri_logo"
+          width="100"
+          height="100"
+          alt="logo"
+        />
+      </div>
+      <div>
+        {img === null ? (
+          <img
+            src={Example}
+            className="upload_picture"
+            width="200"
+            height="320"
+            alt="upload"
+          />
+        ) : (
+          <img
+            src={img}
+            className="upload_picture"
+            width="200"
+            height="320"
+            alt="upload"
+          />
+        )}
+      </div>
+      {value === "풀이과정등록" ? (
+        <input
+          type="submit"
+          value={value}
+          className="submit-button"
+          onClick={handlebutton}
+        />
       ) : (
-        <img src={img} className="upload_picture" width="200" height="320" alt="upload"/>
-      )}
         <form>
-          <input type="file" name="file" onChange={uploadFile}/>
-          {value==="풀이과정등록" ? (
-            <input type="submit" value={value} className="submit-button" onClick={(e)=>e.preventDefault()}/>
-          ) : (
-            <input type="submit" value={value} className="submit-button" onClick={handleSubmit}/>
-          )}
+          <div>
+            <input
+              type="file"
+              className="input-file"
+              name="file"
+              onChange={uploadFile}
+            />
+          </div>
+          <input
+            type="submit"
+            value={value}
+            className="submit-button"
+            onClick={handleSubmit}
+          />
         </form>
+      )}
+      {result ? <Route path="/result" /> : ""}
     </div>
   );
 }
-
-
 
 export default Main;
