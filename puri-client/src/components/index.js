fca2542b3606a36c5062a336e23bce92db2c139b
Myunggyun <sonmg90@gmail.com>
2020-08-15 11:29:19 +0900

    missed file

---
+++ puri-client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Index } from '../components/Index/index';
+export { default as Main } from '../components/Main/main';
+export { default as Result } from '../components/Result/result';
