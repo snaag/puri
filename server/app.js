569e8b6b6a67331583c49a5215a7710d852b048c
snaag <42943992+snaag@users.noreply.github.com>
2020-08-19 14:36:48 +0900

    [MERGE] 명균님, 진영님, 상아님 코드 합침 (#28)

--- server/app.js
+++ server/app.js
@@ -2,16 +2,16 @@ const express = require("express");
 const cors = require("cors");
 
 //add
-const aws =require("aws-sdk");
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-aws.config.loadFromPath(__dirname+ "/config/awsconfig.json");
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+aws.config.loadFromPath(__dirname + "/config/awsconfig.json");
 let s3 = new aws.S3();
-const path = require("path")
+const path = require("path");
 //
 
 const user = require("./routes/user");
-const picture = require('./controllers/pictures');
+const picture = require("./controllers/pictures");
 const note = require("./routes/note");
 const notes = require("./routes/notes");
 
@@ -34,55 +34,61 @@ app.get("/", (request, response) => {
 ///사진업로드 => S3 => DB note => 사진 암호값 => Mathpix => 텍스트화 시키기
 
 app.use("/user", user);
-app.post("/result", picture.SendToMathpix)
+app.post("/result", picture.SendToMathpix);
 
 app.use("/note", note);
 app.use("/notes", notes);
 
 //add
 let upload = multer({
-  storage:multerS3({
-    s3:s3,
-    bucket:"purireviewnote",
-    key:function(req, file, cb){
+  storage: multerS3({
+    s3: s3,
+    bucket: "purireviewnote",
+    key: function(req, file, cb) {
       //username + date.now()
       let extension = path.extname(file.originalname);
-      cb(null, Date.now().toString()+extension)
+      cb(null, Date.now().toString() + extension);
     },
-    acl:'public-read-write'
+    acl: "public-read-write"
   })
-})
+});
 
-let location = '';
+let location = "";
 let imgFile;
-app.post("/upload", upload.single("file"), function(req, res, next){
+app.post("/upload", upload.single("file"), function(req, res, next) {
   imgFile = req.file;
-  location = imgFile.location
-  res.json(imgFile)
+  location = imgFile.location;
+  res.json(imgFile);
 });
 
 // app.use("/upload", user)
 
-app.get("/upload", function(req, res, next){
-  s3.listObjects({
-    Bucket:"purireviewnote",
-  }, (err, data)=>{
-    if(err){
-      return console.log(err);
+app.get("/upload", function(req, res, next) {
+  s3.listObjects(
+    {
+      Bucket: "purireviewnote"
+    },
+    (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+      data.location = location;
+      res.json(data);
     }
-    data.location = location;
-    res.json(data);
-  })
+  );
 });
 
-app.post("/delete", function (req, res, next){
-  s3.deleteObject({
-    Bucket:"purireviewnote",
-    Key: imgFile.key
-  }, (err, data)=>{
-    if(err){
-      return console.log(err);
+app.post("/delete", function(req, res, next) {
+  s3.deleteObject(
+    {
+      Bucket: "purireviewnote",
+      Key: imgFile.key
+    },
+    (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.end();
     }
-    res.end()
-  })
-})
+  );
+});
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 const userController = require("../controllers/users");
-const uploadController = require("../controllers/upload")
 const { request, response } = require("express");
 
 // user/join
