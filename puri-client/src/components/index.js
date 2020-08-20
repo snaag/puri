e27a409dba9abf82a62feecdaed6036b57b8bdc9
CreamPuffx2 <exploit017@gmail.com>
2020-08-21 04:34:58 +0900

    [REMOVE] .. 불필요한 파일 삭제

--- puri-client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Index } from './Index/Index';
-export { default as Main } from './Main/Main';
-export { default as Result } from './Result/Result';
-export { default as Navigation } from './Navigation/Navigation';
-export { default as ReviewNote } from './ReviewNote/RevieNote';
